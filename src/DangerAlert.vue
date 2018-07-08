21e46f6355737f97efff9952ee77f060b56b9024
Ada <adziklipa@gmail.com>
2018-07-08 21:04:27 +0100

    Added toggle button with fading

---
+++ src/DangerAlert.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="alert alert-danger">
+        Danger Alert
+    </div>
+</template>
---
+++ src/SuccessAlert.vue
@@ -0,0 +1,7 @@
+<template>
+
+    <div class="alert alert-success">
+       Success Alert
+    </div>
+
+</template>
